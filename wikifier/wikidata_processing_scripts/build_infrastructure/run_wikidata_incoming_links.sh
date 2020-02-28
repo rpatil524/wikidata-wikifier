f077eee3417c8d215a0bf4fe81d7eefc37d3d9f9
saggu <amandeep.s.saggu@gmail.com>
2020-02-28 11:25:26 -0800

    read from zipped file

--- wikifier/wikidata_processing_scripts/build_infrastructure/run_wikidata_incoming_links.sh
+++ wikifier/wikidata_processing_scripts/build_infrastructure/run_wikidata_incoming_links.sh
@@ -3,5 +3,5 @@ spark-submit --master local[24] \
 --executor-memory  32g \
  --py-files /lfs2/amandeep_wikidata/github/wikidata-wikifier/wikifier/wikidata_processing_scripts/build_infrastructure/wikidata_incoming_links.py \
 wikidata_incoming_links.py \
-/data02/amandeep_wikidata/data/wikidata_infrastructure_files_2/raw \
+/data02/amandeep_wikidata/data/wikidata_infrastructure_files_2/raw/latest-all.json.bz2 \
 /data02/amandeep_wikidata/data/wikidata_infrastructure_files_2/wikidata_incoming_links
