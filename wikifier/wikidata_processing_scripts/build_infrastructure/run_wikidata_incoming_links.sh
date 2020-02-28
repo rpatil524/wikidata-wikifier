87652b00aefa7fb00ab40378847fd98fa58d2e33
saggu <amandeep.s.saggu@gmail.com>
2020-02-28 11:30:30 -0800

    corrected file path

--- wikifier/wikidata_processing_scripts/build_infrastructure/run_wikidata_incoming_links.sh
+++ wikifier/wikidata_processing_scripts/build_infrastructure/run_wikidata_incoming_links.sh
@@ -1,7 +1,7 @@
 spark-submit --master local[24] \
 --driver-memory 32g \
 --executor-memory  32g \
- --py-files /lfs2/amandeep_wikidata/github/wikidata-wikifier/wikifier/wikidata_processing_scripts/build_infrastructure/wikidata_incoming_links.py \
+ --py-files /data02/amandeep_wikidata/github/wikidata-wikifier/wikifier/wikidata_processing_scripts/build_infrastructure/wikidata_incoming_links.py \
 wikidata_incoming_links.py \
 /data02/amandeep_wikidata/data/wikidata_infrastructure_files_2/raw/latest-all.json.bz2 \
 /data02/amandeep_wikidata/data/wikidata_infrastructure_files_2/wikidata_incoming_links
