a47799540afc243a62c0e6f1a367df77871bbc2d
saggu <amandeep.s.saggu@gmail.com>
2020-03-02 13:29:15 -0800

    convert json line to xompressed zip

---
+++ wikifier/wikidata_processing_scripts/build_infrastructure/create_incoming_links_dict.py
@@ -0,0 +1,20 @@
+import json
+import glob
+import gzip
+
+input_path = '/data02/amandeep_wikidata/data/wikidata_infrastructure_files_2/wikidata_incoming_links'
+
+d = dict()
+c = 0
+for f in glob.glob('{}/part-*'.format(input_path)):
+    print('processing file: {}'.format(f))
+    lines = open(f)
+    for line in lines:
+        if c % 10000 == 0:
+            print('done with {} lines'.format(c))
+        c += 1
+        x = json.loads(line)
+        d[x['qnode']] = x['incoming_links']
+
+gzip.open('incoming_links_dictionary.json.gz', 'w').write(json.dumps(d))
+print('Done')
